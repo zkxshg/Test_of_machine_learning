3269ea1b203f38db19af13bf9debf102def54d77
zkxshg <35519607+zkxshg@users.noreply.github.com>
2019-10-12 23:50:35 +0800

    Create Test_TreePrune.py

---
+++ Test_TreePrune.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn as ml
+
+# 查看决策树范例
+ml.plots.plot_animal_tree()
+plt.show()
+
+# 从scikit-learn工具包的datasets包来加载数据集：
+from sklearn.datasets import load_breast_cancer
+# 导入癌症诊断数据集
+cancer = load_breast_cancer()
+
+# 导入划分训练集和测试集的工具包
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+# 通过预剪枝将树的高度限定为4层
+tree = DecisionTreeClassifier(max_depth=4, random_state=0)
+tree.fit(X_train, y_train)
+print(tree)
+print("Accuracy on training set: {:.3f}".format(tree.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(tree.score(X_test, y_test)))
+
+# 导入sklearn决策树可视化工具包
+from sklearn.tree import export_graphviz
+# 将绘制得到的决策树模型保存为dot格式图形文件
+export_graphviz(tree, out_file="tree-pru.dot", class_names=["malignant","benign"],
+                feature_names=cancer.feature_names, impurity=False, filled=True)
+
+# 通过graphviz工具包绘制生成的决策树模型
+import graphviz
+with open("tree-pru.dot") as f:
+    dot_graph = f.read()
+
+gra = graphviz.Source(dot_graph)
+gra.view()
